0ec12d9f57443b3e908a777cfbaf6bcb673c2c1a
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-05 14:12:22 -0400

    complete

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,15 @@
-class Model
-  # Replace with Dog clas
+class Dog
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
+
+  def self.all
+    DOGS
+  end
+
 end
