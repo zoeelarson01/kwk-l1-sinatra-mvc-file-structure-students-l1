9b12a2afad434e02005fab0aafce5a0e2fae8a83
zoeelarson01 <zoeelarson@gmail.com>
2018-06-15 17:23:04 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,61 +1,61 @@
- require_relative 'app/models/models.rb'
-class Model
+# require_relative 'app/models/models.rb'
+# class Model
  
 
 
-      # vegan=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "Ben & Jerry’s", "Häagen Dazs", "Halo Top", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Cashewtopia"]
-      # peanut=["SO Delicious", "Luna & Larry’s Coconut Bliss","Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Graeters"]
-      # tree_nut=["Luna & Larry’s Coconut Bliss","Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Graeters"]
-      # milk=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "Ben & Jerry’s", "Häagen Dazs", "Halo Top", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "Cashewtopia", "Arctic Zero", "Graeters"]
-      # egg=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "Ben & Jerry’s", "Häagen Dazs", "Halo Top", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Cashewtopia", "Graeters"]
-      # soy=["SO Delicious", "Luna & Larry’s Coconut Bliss", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Cashewtopia", "Graeters"]
-      # gluten_free=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Cashewtopia", "Graeters"]
+#       # vegan=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "Ben & Jerry’s", "Häagen Dazs", "Halo Top", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Cashewtopia"]
+#       # peanut=["SO Delicious", "Luna & Larry’s Coconut Bliss","Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Graeters"]
+#       # tree_nut=["Luna & Larry’s Coconut Bliss","Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Graeters"]
+#       # milk=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "Ben & Jerry’s", "Häagen Dazs", "Halo Top", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "Cashewtopia", "Arctic Zero", "Graeters"]
+#       # egg=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "Ben & Jerry’s", "Häagen Dazs", "Halo Top", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Cashewtopia", "Graeters"]
+#       # soy=["SO Delicious", "Luna & Larry’s Coconut Bliss", "Talenti Sorbetto", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Cashewtopia", "Graeters"]
+#       # gluten_free=["SO Delicious", "Luna & Larry’s Coconut Bliss","Dream", "Steve’s Ice Cream", "NadaMoo!", "NanaCreme", "Wink Frozen Desserts", "A La Mode", "Breyers", "Cashewtopia", "Graeters"]
 
-      # l = []
-      # x=0
-      # t = vegan.length
-      # loop
-      #   f = vegan[x]
-      #   if
+#       # l = []
+#       # x=0
+#       # t = vegan.length
+#       # loop
+#       #   f = vegan[x]
+#       #   if
 
-def results("vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free")
-ice_cream_brands={
-"So Delicious"=>["vegan", "peanut free", "soy free", "milk free", "egg free", "gluten free"]
-"Luna & Larry's Coconut Bliss"=>["vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free", "organic"]
-"Dream"=>["vegan", "milk free", "egg free", "gluten free"]
-"Steve's Ice Cream"=>["vegan", "milk free", "egg free", "gluten free"]
-"Ben & Jerry's"=>["vegan", "milk free", "egg free"]
-"Häagen Dazs"=>["vegan", "milk free", "egg free"]
-"Halo Top"=>["vegan", "milk free", "egg free"]
-"Talenti Sorbetto"=>["vegan", "peanut free", "soy free", "milk free", "egg free"]
-"Nadamoo!"=>["vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free", "organic"]
-"NanaCreme" =>["vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free"]
-"Wink Frozen Desserts"=>["peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free"]
-"A La Mode"=>["peanut free", "tree nut free", "egg free", "soy free", "gluten free"]
-"Breyers"=>["peanut free", "tree nut free", "soy free", "gluten free"]
-"Cashewtopia"=>["vegan", "milk free", "egg free", "soy free", "gluten free"]
-"DF Mavens"=>["gluten free"]
-"Artic Zero"=>["milk free", "gluten free"]
-"Graeters"=>["peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free"]
-}
+# def results("vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free")
+# ice_cream_brands={
+# "So Delicious"=>["vegan", "peanut free", "soy free", "milk free", "egg free", "gluten free"]
+# "Luna & Larry's Coconut Bliss"=>["vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free", "organic"]
+# "Dream"=>["vegan", "milk free", "egg free", "gluten free"]
+# "Steve's Ice Cream"=>["vegan", "milk free", "egg free", "gluten free"]
+# "Ben & Jerry's"=>["vegan", "milk free", "egg free"]
+# "Häagen Dazs"=>["vegan", "milk free", "egg free"]
+# "Halo Top"=>["vegan", "milk free", "egg free"]
+# "Talenti Sorbetto"=>["vegan", "peanut free", "soy free", "milk free", "egg free"]
+# "Nadamoo!"=>["vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free", "organic"]
+# "NanaCreme" =>["vegan", "peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free"]
+# "Wink Frozen Desserts"=>["peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free"]
+# "A La Mode"=>["peanut free", "tree nut free", "egg free", "soy free", "gluten free"]
+# "Breyers"=>["peanut free", "tree nut free", "soy free", "gluten free"]
+# "Cashewtopia"=>["vegan", "milk free", "egg free", "soy free", "gluten free"]
+# "DF Mavens"=>["gluten free"]
+# "Artic Zero"=>["milk free", "gluten free"]
+# "Graeters"=>["peanut free", "tree nut free", "milk free", "egg free", "soy free", "gluten free"]
+# }
 
-  if ice_cream-brands.keys.include? ("vegan")
-    puts ice_cream_brands.keys
-  elsif ice_cream-brands.keys.include? ("peanut free")
-    puts ice_cream-brands.keys
-  elsif ice_cream-brands.keys.include? ("tree nut free")
-    puts ice_cream-brands.keys
-  elsif ice_cream-brands.keys.include? ("milk free")
-    puts ice_cream-brands.keys
-  elsif ice_cream-brands.keys.include? ("egg free")
-    puts ice_cream-brands.keys
-  elsif ice_cream-brands.keys.include? ("soy free")
-    puts ice_cream-brands.keys
-  elsif ice_cream-brands.keys.include? ("gluten free")
-    puts ice_cream-brands.keys
-  else
-  end
+#   if ice_cream-brands.keys.include? ("vegan")
+#     puts ice_cream_brands.keys
+#   elsif ice_cream-brands.keys.include? ("peanut free")
+#     puts ice_cream-brands.keys
+#   elsif ice_cream-brands.keys.include? ("tree nut free")
+#     puts ice_cream-brands.keys
+#   elsif ice_cream-brands.keys.include? ("milk free")
+#     puts ice_cream-brands.keys
+#   elsif ice_cream-brands.keys.include? ("egg free")
+#     puts ice_cream-brands.keys
+#   elsif ice_cream-brands.keys.include? ("soy free")
+#     puts ice_cream-brands.keys
+#   elsif ice_cream-brands.keys.include? ("gluten free")
+#     puts ice_cream-brands.keys
+#   else
+#   end
 
-end
+# end
 
-end
+# end
